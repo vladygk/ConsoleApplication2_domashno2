d8f2e3c10c5efc33b66724fd5613e832a8fdc0a2
vladygk <vladygk8@gmail.com>
2016-11-10 03:01:12 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PrintASCII
+{
+    class PrintTheASCIItable
+    {
+        static void Main()
+        {
+            int number;
+            for(number=33;number<=126;number++)
+                if(number<=126)
+                {
+                    Console.Write((char)number);
+
+                }
+            
+        }
+    }
+}
